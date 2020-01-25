0a81f06c17cd6986a160c02c4c2155e1eec071a9
isabroch <isabrochweb@gmail.com>
2020-01-25 13:03:15 +0100

    Testing

--- CloneNode/index.js
+++ CloneNode/index.js
@@ -3,30 +3,26 @@ const list = ['list1', 'list2', 'list3', 'list4',]
 const subList = ['sublist', 'sublist', 'sublist', 'sublist',]
 
 list.forEach(element => {
-    const template = document.querySelector('template')
-    const outerTemplate = template.content.cloneNode(true)
-    const li = outerTemplate.querySelector('li')
-    const ul = outerTemplate.querySelector('ul')
-    li.textContent = element
+    const outerTemplate = document.querySelector('template').content.cloneNode();
+    const li = outerTemplate.querySelector('li');
+    const ul = outerTemplate.querySelector('ul');
+    li.prepend(document.createTextNode(element));
 
-    console.log(outerTemplate)
-    
     subList.forEach(element => {
-        const innerTemplate = outerTemplate.querySelector('template').content.cloneNode(true)
-        const li = innerTemplate.querySelector('li')
-        li.textContent = element
-        ul.appendChild(innerTemplate)
+        const innerTemplate = outerTemplate.querySelector('template').content.cloneNode();
+        const li = innerTemplate.querySelector('li');
+        li.append(document.createTextNode(element));
+        ul.appendChild(innerTemplate);
     })
-    
-    //sdocument.querySelector('ul').appendChild(outerTemplate)
-    document.querySelector('ul').appendChild(li).appendChild(ul)
+
+    document.querySelector('ul').appendChild(outerTemplate);
 });
 
 // ul #
 
 // template *
 //     li
-//     ul 
+//     ul
 
 // template
 //     li
